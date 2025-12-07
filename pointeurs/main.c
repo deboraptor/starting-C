7adb36d42e7af0da0ff7850b4ce2a90aceb5ed67
deboraptor <vdzdebora@gmail.com>
2025-12-07 13:05:05 +0100

    :sparkles: add Makefile and initial main.c for pointer example

---
+++ pointeurs/main.c
@@ -0,0 +1,12 @@
+// https://openclassrooms.com/fr/courses/19980-apprenez-a-programmer-en-c/7672176-creez-et-initialisez-des-pointeurs
+
+#include <stdio.h>
+
+void main()
+{
+    //int variable = 5;
+    //printf("variable = %d\n", &variable);
+    //printf("adresse variable = %p\n", &variable);
+
+    int *monPointeur = NULL; // ou int* monPointeur
+}
