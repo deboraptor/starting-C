2054079445b138d7235445cff650507964d4a45d
deboraptor <vdzdebora@gmail.com>
2025-12-09 17:26:19 +0100

    :sparkles: my first array - add Makefile and main.c

---
+++ tableaux/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int tableau[4];
+
+    tableau[0] = 10;
+    tableau[1] = 23;
+    tableau[2] = 505;
+    tableau[3] = 8;
+
+    printf("case 3 = %d\n", tableau[2]);
+}
