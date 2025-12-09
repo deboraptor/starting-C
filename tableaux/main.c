324c99cb8d04b2ddd95d3963ea208ef702264f08
deboraptor <vdzdebora@gmail.com>
2025-12-09 18:46:38 +0100

    :sparkles: enhance main.c with additional array examples and pointer usage

--- tableaux/main.c
+++ tableaux/main.c
@@ -1,3 +1,5 @@
+// https://openclassrooms.com/fr/courses/19980-apprenez-a-programmer-en-c/7672506-creez-des-variables-de-type-tableaux
+
 #include <stdio.h>
 
 int main()
@@ -9,5 +11,16 @@ int main()
     tableau[2] = 505;
     tableau[3] = 8;
 
-    printf("case 3 = %d\n", tableau[2]);
+    int a[3] = {1, 2, 3};  // deuxième manière
+
+    printf("pointeur de tableau = %d\n", tableau);
+    printf("première valeur de tableau = %d\n", *tableau); // ça marche vu que c'est un pointeur (pour la première valeur)
+
+    /*
+    tableau[1] // Renvoie la valeur de la seconde case (la première case étant 0)
+    *(tableau + 1) // Identique : renvoie la valeur contenue dans la seconde case   
+    */
+
+    printf("case 3 de tableau = %d\n", tableau[2]);
+    printf("case 2 de a = %d\n", a[1]);
 }
