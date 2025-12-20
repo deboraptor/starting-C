6cda094b451152a3b9b7196b73159f2ba354a49e
deboraptor <vdzdebora@gmail.com>
2025-12-20 23:00:32 +0100

    :sparkles: makes isPalindrome function working with actual palindromes

--- palindrome/main.c
+++ palindrome/main.c
@@ -10,9 +10,10 @@ mots « kayak », « radar », « coloc », « rêver », « ressasser ».
 #include <stdio.h>
 #include <string.h>
 
-int isCoucou(char *);
+int isCoucou(char *, char *);
 int lenIs(char *);
 void ask4String(char *);
+int isPalindrome(char *);
 
 int main(void)
 {   
@@ -20,21 +21,18 @@ int main(void)
 
     ask4String(string);
 
-    int res = isCoucou(string);
-    printf("Typed word is 'coucou': %d\n", res);
+    //int res = isCoucou(string);
+    //printf("Typed word is 'coucou': %d\n", res);
 
     int len = lenIs(string);
-    printf("Longueur = %d\n", len);
+    //printf("Longueur = %d\n", len);
+
+    isPalindrome(string);
 }
 
-int isCoucou(char *string)
-{
-    char coucou[] = "coucou";    
-    
-    //printf("mot: %s\n", mot);
-    //printf("%s\n", coucou);
-
-    int cmp = strcmp(string, coucou);
+int isCoucou(char *a, char *b)
+{    
+    int cmp = strcmp(a, b);
     //printf("cmp: %d\n", cmp);
 
     if (cmp == 0)
@@ -51,7 +49,7 @@ int lenIs(char *string)
 {
     int len = 0;
 
-    len = strlen(string);
+    len = strlen(string); // TODO: retourne un size_t, à améliorer
 
     return len;
 }
@@ -60,4 +58,48 @@ void ask4String(char *string)
 {
     printf("Entrez un mot: ");
     scanf("%50s", string); // %Ns -> bloquer à 50 caractères max | pas &mot car le nom d’un tableau représente déjà son adresse
+}
+
+int isPalindrome(char *string)
+{
+    size_t taille = lenIs(string);
+    int i;
+    long unsigned int mid = taille / 2;
+    //printf("dernière lettre = %c\n", string[taille-1]);
+
+    /*
+    si 
+    debut à moitié (on stocke)
+
+    et 
+    moitié à fin (inversé)
+
+    est égal -> palindrome
+    */
+
+    char a[51];
+    for (i = 0; i <= mid; i++)
+    {   
+        a[i] = string[i];
+        //printf("%c\n", string[i]);
+    }
+
+    char b[51];
+    for (i = mid; i < taille; i++)
+    {   
+        b[i] = string[i];
+        //printf("%c\n", string[i]);
+    }
+
+    char inverse[51];
+    for (i = 0; i < taille; i++)
+    {   
+        inverse[i] = b[taille-1-i];
+    }
+
+    //printf("%s\n", inverse);
+    inverse[taille] = '\0';
+    //printf("a = %s\n", a);
+    //printf("inverse =%s\n", inverse);
+    printf("Palindrome ? %d\n", isCoucou(a, inverse));
 }
