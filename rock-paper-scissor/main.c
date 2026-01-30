34142aca2afb742d25bf3f1adf1b073a7b732dd0
deboraptor <vdzdebora@gmail.com>
2026-01-30 22:24:54 +0100

    :bulb: add code structure

--- rock-paper-scissor/main.c
+++ rock-paper-scissor/main.c
@@ -5,3 +5,14 @@ int main(void) {
 
   return 0;
 }
+
+// fonction askUser
+// variable userAnswer
+
+// fonction askComputer
+// variable computerAnswer
+// -> génère de manière random un nombre
+// qui correspond à Rock Paper Scissor
+
+// fonction game
+// -> pour savoir qui gagne !
